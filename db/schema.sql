c25b4001c39c817eaf7b0e72a92473780138ee94
mikekenn <michaelkennedy_228@hotmail.com>
2018-02-13 13:43:43 -0400

    Finished replacing tabs with spaces.

--- db/schema.sql
+++ db/schema.sql
@@ -30,135 +30,135 @@ GRANT SELECT ON visits TO PUBLIC;
 DROP TABLE mri_scans;
 CREATE TABLE mri_scans
 (
-	id					INTEGER			NOT NULL,
-	technician_notes	VARCHAR2,	
-	technician_id		INTEGER,
-	measurements 		measurement_set NOT NULL,
-	visit_id			INTEGER			NOT NULL,
-	device_id			INTEGER,
-	
-	PRIMARY KEY(id);
-	
-	CONSTRAINT visit_id FOREIGN KEY(visit_id) REFERENCES visits(id),
-	CONSTRAINT technician_id FOREIGN KEY(technician_id); REFERENCES technicians(id),
-	CONSTRAINT device_id FOREIGN KEY(device_id); REFERENCES devices(id),
+    id                  INTEGER            NOT NULL,
+    technician_notes    VARCHAR2,
+    technician_id       INTEGER,
+    measurements        measurement_set    NOT NULL,
+    visit_id            INTEGER            NOT NULL,
+    device_id           INTEGER,
+    
+    PRIMARY KEY(id);
+    
+    CONSTRAINT visit_id FOREIGN KEY(visit_id) REFERENCES visits(id),
+    CONSTRAINT technician_id FOREIGN KEY(technician_id); REFERENCES technicians(id),
+    CONSTRAINT device_id FOREIGN KEY(device_id); REFERENCES devices(id),
 );
 GRANT SELECT ON mri_scans TO PUBLIC;
 
 DROP TABLE diagnoses;
 CREATE TABLE diagnoses
 (
-	id 				INTEGER		NOT NULL,
-	condition_id	INTEGER		NOT NULL,
-	patient_number	INTEGER		NOT NULL,
-	physician_id	INTEGER		NOT NULL,
-	test_results	VARCHAR2(10),
-	date			TIMESTAMP,
-	change			VARCHAR2(5),
-	
-	PRIMARY KEY (id);
-	
-	CONSTRAINT condition_id  FOREIGN KEY(condition_id); REFERENCES conditions(id),
-	CONSTRAINT patient_number  FOREIGN KEY(patient_number); REFERENCES patients(patient_number),
-	CONSTRAINT physician_id FOREIGN KEY(physician_id); REFERENCES physicians(id),
+    id              INTEGER        NOT NULL,
+    condition_id    INTEGER        NOT NULL,
+    patient_number  INTEGER        NOT NULL,
+    physician_id    INTEGER        NOT NULL,
+    test_results    VARCHAR2(10),
+    date            TIMESTAMP,
+    change          VARCHAR2(5),
+    
+    PRIMARY KEY (id);
+    
+    CONSTRAINT condition_id  FOREIGN KEY(condition_id); REFERENCES conditions(id),
+    CONSTRAINT patient_number  FOREIGN KEY(patient_number); REFERENCES patients(patient_number),
+    CONSTRAINT physician_id FOREIGN KEY(physician_id); REFERENCES physicians(id),
 );
 GRANT SELECT ON diagnoses TO PUBLIC;
 
 DROP TABLE regimens;
 CREATE TABLE regimens
 (
-	id				INTEGER,
-	physician_id	INTEGER,
-	start_date		TIMESTAMP,
-	
-	PRIMARY KEY (id);
-	
-	CONSTRAINT physician_id FOREIGN KEY(physician_id) REFERENCES physicians(id),
+    id              INTEGER,
+    physician_id    INTEGER,
+    start_date      TIMESTAMP,
+    
+    PRIMARY KEY (id);
+    
+    CONSTRAINT physician_id FOREIGN KEY(physician_id) REFERENCES physicians(id),
 );
 GRANT SELECT ON regimens TO PUBLIC;
 
 DROP TABLE measurements;
 CREATE TABLE measurements
 (
-	id				INTEGER,
-	label			VARCHAR2(20),
-	region			VARCHAR2(20)		UNIQUE,
-	
-	PRIMARY KEY (id);
-	
-	CONSTRAINT physician_id FOREIGN KEY(physician_id) REFERENCES physicians(id),
+    id              INTEGER,
+    label           VARCHAR2(20),
+    region          VARCHAR2(20)        UNIQUE,
+    
+    PRIMARY KEY (id);
+    
+    CONSTRAINT physician_id FOREIGN KEY(physician_id) REFERENCES physicians(id),
 );
 GRANT SELECT ON regimens TO PUBLIC;
 
 DROP TABLE genomes;
 CREATE TABLE genomes
 (
-	id				INTEGER,
-	sequence		BINARY,
-	date_taken		TIMESTAMP,
-	visit_id		TIMESTAMP,
-	
-	PRIMARY KEY (id);
-	
-	CONSTRAINT visit_id FOREIGN KEY(visit_id) REFERENCES visits(id),
+    id              INTEGER,
+    sequence        BINARY,
+    date_taken      TIMESTAMP,
+    visit_id        TIMESTAMP,
+    
+    PRIMARY KEY (id);
+    
+    CONSTRAINT visit_id FOREIGN KEY(visit_id) REFERENCES visits(id),
 );
 GRANT SELECT ON genomes TO PUBLIC;
 
 DROP TABLE conditions;
 CREATE TABLE conditions
 (
-	id						INTEGER,
-	similar_conditions		VARCHAR2(50),
-	name					VARCHAR2(50),
-	signs					VARCHAR2(50),
-	symptoms				VARCHAR2(2000),
-	
-	PRIMARY KEY (id);
+    id                      INTEGER,
+    similar_conditions      VARCHAR2(50),
+    name                    VARCHAR2(50),
+    signs                   VARCHAR2(50),
+    symptoms                VARCHAR2(2000),
+    
+    PRIMARY KEY (id);
 );
 GRANT SELECT ON conditions TO PUBLIC;
 
 DROP TABLE treatments;
 CREATE TABLE treatments
 (
-	id		INTEGER,
-	type	VARCHAR2(30),
-	
-	PRIMARY KEY (id);
+    id      INTEGER,
+    type    VARCHAR2(30),
+    
+    PRIMARY KEY (id);
 );
 GRANT SELECT ON treatments TO PUBLIC;
 
 DROP TABLE devices;
 CREATE TABLE devices
 (
-	id				INTEGER,
-	manufacturer	VARCHAR2(20),
-	model_number	VARCHAR2(15),
-	
-	PRIMARY KEY (id);
+    id              INTEGER,
+    manufacturer    VARCHAR2(20),
+    model_number    VARCHAR2(15),
+    
+    PRIMARY KEY (id);
 );
 GRANT SELECT ON patients TO PUBLIC;
 
 DROP TABLE physicians;
 CREATE TABLE physicians
 (
-	id			INTEGER,
-	first_name	VARCHAR2(20),
-	last_name	VARCHAR2(20),
-	specialty	VARCHAR2(25),
-	
-	PRIMARY KEY (id);
+    id           INTEGER,
+    first_name   VARCHAR2(20),
+    last_name    VARCHAR2(20),
+    specialty    VARCHAR2(25),
+    
+    PRIMARY KEY (id);
 );
 GRANT SELECT ON physicians TO PUBLIC;
 
 DROP TABLE users;
 CREATE TABLE users
 (
-	id					INTEGER,
-	email				VARCHAR2(50),
-	first_name			VARCHAR2(20),
-	last_name			VARCHAR2(20),
-	hipaa_authorized	BOOLEAN,
-	
-	PRIMARY KEY (id);
+    id                  INTEGER,
+    email               VARCHAR2(50),
+    first_name          VARCHAR2(20),
+    last_name           VARCHAR2(20),
+    hipaa_authorized    BOOLEAN,
+    
+    PRIMARY KEY (id);
 );
 GRANT SELECT ON users TO PUBLIC;
