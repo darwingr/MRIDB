9b8c517d4536e549a357914fee3ccdd9edc3190f
Mike <michaelkennedy_228@hotmail.com>
2018-02-24 18:33:44 -0400

    VisitModel class

--- src/models/VisitModel.java
+++ src/models/VisitModel.java
@@ -8,8 +8,12 @@ package models;
  *
  */
 public class VisitModel extends ActiveRecordModel {
-
-	
+	private  int id;
+	private String patientGender;
+	private int patientAge;
+	private String checkIn;
+	private String checkOut;
+	private int patientNumber;	
 	
 	public VisitModel() {
 		// TODO Auto-generated constructor stub
