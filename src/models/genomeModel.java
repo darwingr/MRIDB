949a371d85ec19eb064746d7f16848279628c2ee
Mike <michaelkennedy_228@hotmail.com>
2018-02-24 18:34:42 -0400

    genomeModel model class

---
+++ src/models/genomeModel.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ */
+package models;
+
+/*
+ * @author Michael
+ * 
+ */
+
+public class genomeModel {
+	private int id;
+	private String Sequence;
+	private String dateTaken;
+	private int visitID;
+
+	public genomeModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
