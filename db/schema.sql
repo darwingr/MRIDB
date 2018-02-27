35e2e9bf51a7ac6e8197f8cdb3a9fbc903ba0abe
Mike <michaelkennedy_228@hotmail.com>
2018-02-27 16:46:39 -0400

    Most notable change is to the User table. No boolean type in sqlplus. had to use a work around.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+CREATE TYPE measurement_set AS VARRAY(1125) OF DOUBLE;
 DROP TYPE measurement_set;
 DROP TABLE technicians;
 DROP TABLE visits;
@@ -13,113 +14,112 @@ DROP TABLE devices;
 DROP TABLE users;
 DROP TABLE physicians;
 
-CREATE TYPE measurement_set AS VARRAY(1125) OF DOUBLE;
 
 CREATE TABLE technicians
 (
-    id                      INTEGER(5)         NOT NULL    unique,
+    id                      NUMBER(5)          NOT NULL    unique,
     first_name              VARCHAR(20)        NOT NULL,
     last_name               VARCHAR(20)        NOT NULL
 );
 
 CREATE TABLE patients
 (
-        number              INTEGER(9)         NOT NULL    unique,	
+        patient_number      NUMBER(9)          NOT NULL    unique,	
         first_name          VARCHAR2(20)       NOT NULL,
         last_name           VARCHAR2(20)       NOT NULL
 );
 
 CREATE TABLE visits
 (
-        id                  INTEGER(12)        NOT NULL    unique,
-        gender              int(1)             NOT NULL,
-        check_in            TIMESTAMP          NOT NULL,
+        id                  NUMBER(12)        NOT NULL    unique,
+        gender              NUMBER(1)         NOT NULL,
+        check_in            TIMESTAMP         NOT NULL,
         check_out           TIMESTAMP,
-        patient_number      INTEGER            NOT NULL
+        patient_number      NUMBER            NOT NULL
 );
 
 CREATE TABLE mri_scans
 (
-    id                      INTEGER(12)        NOT NULL    unique,
+    id                      NUMBER(12)        NOT NULL    unique,
     technician_notes        VARCHAR2(100),
-    technician_id           INTEGER,
-    measurements            measurement_set    NOT NULL,
-    visit_id                INTEGER            NOT NULL,
-    device_id               INTEGER
+    technician_id           NUMBER,
+    visit_id                NUMBER            NOT NULL,
+    device_id               NUMBER
 );
+    measurements            measurement_set   NOT NULL,
 
 CREATE TABLE diagnoses
 (
-    id                      INTEGER(10)        NOT NULL    unique,
-    condition_id            INTEGER            NOT NULL,
-    patient_number          INTEGER            NOT NULL,
-    physician_id            INTEGER            NOT NULL,
+    id                      NUMBER(10)        NOT NULL    unique,
+    condition_id            NUMBER            NOT NULL,
+    patient_number          NUMBER            NOT NULL,
+    physician_id            NUMBER            NOT NULL,
     physician_notes         VARCHAR2(200),    
-    date                    TIMESTAMP,
+    diag_date               TIMESTAMP
 );
 
 CREATE TABLE regimens
 (
-    id                      INTEGER(10)        NOT NULL    unique,
-    patient_number          INTEGER            NOT NULL,
-    physician_id            INTEGER,           NOT NULL,
+    id                      NUMBER(10)        NOT NULL    unique,
+    patient_number          NUMBER            NOT NULL,
+    physician_id            NUMBER            NOT NULL,
     physician_notes         VARCHAR(200),
-    start_date              TIMESTAMP          NOT NULL,
-    treatment_id            INTEGER            NOT NULL,
+    start_date              TIMESTAMP         NOT NULL,
+    treatment_id            NUMBER            NOT NULL
 );
 
 CREATE TABLE measurements
 (
-    id                      INTEGER,
-    label                   VARCHAR2(100)      NOT NULL    unique,
+    id                      NUMBER,
+    label                   VARCHAR2(100)     NOT NULL    unique,
     brain_region            VARCHAR2(20)
 );
 
 CREATE TABLE genomes
 (
-    id                      INTEGER(10)        NOT NULL    unique,
-    sequence                VARCHAR(100)       NOT NULL,
-    date_taken              TIMESTAMP          NOT NULL,
-    visit_id                INTEGER            NOT NULL
+    id                      NUMBER(10)        NOT NULL    unique,
+    sequence                VARCHAR(100)      NOT NULL,
+    date_taken              TIMESTAMP         NOT NULL,
+    visit_id                NUMBER            NOT NULL
 );
 
 CREATE TABLE conditions
 (
-    id                      INTEGER(5)         NOT NULL    unique,
-    name                    VARCHAR2(50)       NOT NULL    unique,
+    id                      NUMBER(5)         NOT NULL    unique,
+    name                    VARCHAR2(50)      NOT NULL    unique,
     signs                   VARCHAR2(50),
     symptoms                VARCHAR2(2000)
 );
 
 CREATE TABLE treatments
 (
-    id                      INTEGER(6)         NOT NULL    unique,
-    type                    VARCHAR2(10)       NOT NULL,
-    description             VARCHAR(100)       NOT NULL
+    id                      NUMBER(6)         NOT NULL    unique,
+    type                    VARCHAR2(10)      NOT NULL,
+    description             VARCHAR(100)      NOT NULL
 );
 
 CREATE TABLE devices
 (
-    id                      INTEGER(5)         NOT NULL    unique,
+    id                      NUMBER(5)         NOT NULL    unique,
     manufacturer            VARCHAR2(20),
     model_number            VARCHAR2(20),
-    hospital_location       VARCHAR(20)        NOT NULL
+    hospital_location       VARCHAR(20)       NOT NULL
 );
 
 CREATE TABLE physicians
 (
-    id                      INTEGER(5)         NOT NULL    unique,
-    first_name              VARCHAR2(20)       NOT NULL,
-    last_name               VARCHAR2(20)       NOT NULL,
-    specialty               VARCHAR2(25)       NOT NULL
+    id                      NUMBER            NOT NULL    unique,
+    first_name              VARCHAR2(20)      NOT NULL,
+    last_name               VARCHAR2(20)      NOT NULL,
+    specialty               VARCHAR2(25)      NOT NULL
 );
 
 CREATE TABLE users
 (
-    id                      INTEGER(5)         NOT NULL    unique,
-    username                VARCHAR(45)        NOT NULL    unique,
-    email                   VARCHAR2(50)       NOT NULL    unique,
-    first_name              VARCHAR2(20)       NOT NULL,
-    last_name               VARCHAR2(20)       NOT NULL,
-    hipaa_authorized        BOOLEAN,
+    id                      NUMBER            NOT NULL    unique,
+    username                VARCHAR(45)       NOT NULL    unique,
+    email                   VARCHAR2(50)      NOT NULL    unique,
+    first_name              VARCHAR2(20)      NOT NULL,
+    last_name               VARCHAR2(20)      NOT NULL,
+    hipaa_authorized        NUMBER(1)         NOT NULL CHECK  (hipaa_authorized in (0,1))
 );
