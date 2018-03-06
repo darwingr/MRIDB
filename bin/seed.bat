a5d0326498b6b1ecef2b4231ce0e7b224271e353
Mike <michaelkennedy_228@hotmail.com>
2018-03-06 14:43:59 -0400

    update of seed file.

--- bin/seed.bat
+++ bin/seed.bat
@@ -1,3 +1,5 @@
+%SQLPLUS_PATH% %DB_USERNAME%/%DB_PASSWORD%@csci275 @db\schema.ctl
 for /f %%f in ('dir /b db\loaders\') do (
 	%SQLLDR_PATH% userid=%DB_USERNAME%/%DB_PASSWORD%@csci275 control=db\loaders\%%f log=tmp\sqlldr-%%f.log bad=tmp\sqlldr-bad-%%f.log
 )
+%SQLPLUS_PATH% %DB_USERNAME%/%DB_PASSWORD%@csci275 @db\alter.sql
