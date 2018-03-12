72eaf8e6f9a9586013a04a6869df5637d06976f0
Darwin Groskleg <darwingroskleg@gmail.com>
2018-03-12 00:59:13 -0300

    Adds UserModel with findByID implementation outlined in parent class

--- src/models/ActiveRecord.java
+++ src/models/ActiveRecord.java
@@ -1,28 +1,44 @@
-/**
- * 
- */
 package models;
 
+import adapters.DBAdapter;
+
+import java.sql.ResultSet;
+
 /**
- * @author darwingroskleg
  * This is the intended superclass of all other model objects.
- *
+ * Used as an abstract class, there is no instance of a active record.
+ * Note that all subclasses must have TABLE_NAME initialized.
  */
-//public abstract class ActiveRecord {
 public class ActiveRecord {
-	
-	public static ActiveRecord findById(int id) {
+	private static final String TABLE_NAME = "records";
+
+	/*
+	 * Usage in a subclass: user = UserModel.findByID(5);
+	 */
+	public static ActiveRecord findByID(int id) {
+		DBAdapter db = new DBAdapter();
+		ResultSet result = db.executeQuery("select * from " + TABLE_NAME + " where id = " + id);
+		
+		// TODO
+		// Something here to create the object from the given result set.
+		// Just passing the result set to the object leaves a lot of duplicate work to be done in each
+		// subclass.
+		
 		ActiveRecord record = new ActiveRecord();
 		return record;
 	}
 	
 	public static ActiveRecord findBy(String attribute, int id) {
+		DBAdapter db = new DBAdapter();
+		ResultSet result = db.executeQuery("select * from " + TABLE_NAME + " where " + attribute + "=" + id);
+		
+		// TODO
+		// Same as in findByID above.
+
 		ActiveRecord record = new ActiveRecord();
 		return record;
 	}
 	
-	private String table_name;
-
 	public ActiveRecord() {
 		//constructor
 	}
---
+++ src/models/UserModel.java
@@ -0,0 +1,8 @@
+package models;
+
+public class UserModel extends ActiveRecord {
+	private static final String TABLE_NAME = "users";
+	
+	public UserModel() {
+	}
+}
