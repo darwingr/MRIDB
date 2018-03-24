4d0d655e39c804714961e8ec28cfdff956353043
Mike <michaelkennedy_228@hotmail.com>
2018-03-24 14:19:08 -0300

    Added file.
    Added a .sql file for views we are creating.

--- bin/seed.bat
+++ bin/seed.bat
@@ -5,3 +5,4 @@ for /f %%f in ('dir /b db\loaders\') do (
 	%SQLLDR_PATH% userid=%DB_USERNAME%/%DB_PASSWORD%@csci275 control=db\loaders\%%f log=tmp\sqlldr-%%f.log bad=tmp\sqlldr-bad-%%f.log
 )
 %SQLPLUS_PATH% %DB_USERNAME%/%DB_PASSWORD%@csci275 @db\alter.sql
+%SQLPLUS_PATH% %DB_USERNAME%/%DB_PASSWORD%@csci275 @db\view.sql
