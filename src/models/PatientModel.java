4bd365e07b3e985f724b78f519bc7e07642ed228
Darwin Groskleg <darwingroskleg@gmail.com>
2018-03-31 15:31:30 -0300

    Adds a special delete method to PatientModel for handling visits constraints. Adds create(), constructors and findByID()

--- src/models/PatientModel.java
+++ src/models/PatientModel.java
@@ -1,11 +1,6 @@
-/**
- * 
- */
 package models;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Date;
+import java.util.ArrayList;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,86 +11,111 @@ import adapters.DBAdapter;
  *
  */
 public class PatientModel extends ActiveRecord {
-	private static final String TABLE_NAME = "patients";
+	public String TABLE_NAME = "patients";
 
-	private static int 	   id;
-	private static String first_name;
-	private static String last_name;
-	private static String address;
-	private static String gender;
-	private static Date dob;
+	private int 	   id;
+	private String first_name;
+	private String last_name;
+	private String address;
 
-	/**
-	 * 
-	 */
-	public PatientModel(String fname,String lname) {
-		setFirst_name(fname);
-		setLast_name(lname);
+
+	public static PatientModel findByID(int patient_id) throws SQLException {
+		PatientModel patient = new PatientModel();
+		DBAdapter db = new DBAdapter();
+		try (ResultSet rs = db.executeQuery("select * from " + patient.TABLE_NAME + " where id = " + patient_id)) {
+			if (rs.next()) {
+				patient.id = rs.getInt("id");
+				patient.first_name = rs.getString("first_name");
+				patient.last_name = rs.getString("last_name");
+				patient.address = rs.getString("address");
+			}
+		} catch (SQLException sqle) {
+            System.err.println("Exception occurred while processing Building ResultSet after findByID.");
+		} finally {
+			db.close();
+		}
+		return patient;
 	}
+
+	public PatientModel() {
+		this("Jane", "Doe");
+	}
+
+	public PatientModel(String fname, String lname) {
+		first_name = fname;
+		last_name = lname;
+	}
+
+	@Override
+	public String table() { return "patients"; }
 	
 	public String fullName() {
-		return getFirst_name() + getLast_name();
+		return first_name + ' ' + last_name;
 	}
 
-	public static int getId() {
+	/*
+	 * Creates the current instance of patient as a new record in the database,
+	 * while also signing them in for a visit for the current time.
+	 */
+	public boolean create() throws SQLException {
+		boolean success = false;
+		DBAdapter db = new DBAdapter();
+		boolean patientExists = false;
+		String patient_query = 
+				"SELECT id \n" + //count(first_name), count(last_name)\n" + 
+				"FROM " + table() + '\n' +
+				"WHERE first_name = '" + first_name + "' " + 
+				"AND last_name = '" + last_name + "'";
+		try (ResultSet rs = db.executeQuery(patient_query)) {
+			patientExists = rs.next();
+		}
+		if(!patientExists) {
+			String plsql =
+					"DECLARE \n" +
+					"  rec_id NUMBER; \n" +
+					"BEGIN \n" +
+					"  INSERT INTO " + table() + " \n" + 
+					"    (first_name, last_name) \n" +
+					"    VALUES ('" + first_name + "', '" + last_name + "') \n" +
+					"    RETURNING id INTO rec_id;\n" +
+					"  ? := rec_id;" +
+					"END; \n";
+
+			try {
+				int rec_id = db.executeCall(plsql);
+				if (rec_id != -1) {
+					success = true;
+					id = rec_id;
+
+					// creates a visit with the default dob and gender,
+					// checking in for a visit immediately
+					VisitModel visit = new VisitModel();
+					visit.create(id);
+				}
+			} catch (Exception sqle) {
+	            System.err.println("Exception occurred while processing returning the new record id.");
+			}
+		} else {
+			System.out.println("Patient exists!");
+		}
+		db.close();
+		return success;
+	}
+
+	@Override
+	public boolean delete() throws SQLException {
+		ArrayList<VisitModel> patient_visits = VisitModel.findByPatientID(id);
+		for (VisitModel v : patient_visits) {
+			v.delete(); // TODO Error checking on patient visit deletes
+		}
+		return super.delete();
+	}
+
+	public int getID() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public static String getFirst_name() {
-		return first_name;
-	}
-
-	public void setFirst_name(String first_name) {
-		PatientModel.first_name = first_name;
-	}
-
-	public static String getLast_name() {
-		return last_name;
-	}
-
-	public void setLast_name(String last_name) {
-		PatientModel.last_name = last_name;
-	}
-
-	public static String getAddress() {
+	public String getAddress() {
 		return address;
 	}
-
-	public static void setAddress(String address) {
-		PatientModel.address = address;
-	}
-	
-	public create() throws SQLException {
-		DBAdapter db = new DBAdapter();
-		boolean userExists = false;
-		String sql = 
-				"SELECT count(first_name), count(last_name)\n" + 
-				"FROM users " + 
-				"WHERE first_name = '" + first_name + "' " + 
-				"AND last_name = '" + last_name + "';";
-		try (ResultSet rs = db.executeQuery(sql)) {
-			userExists = rs.next();
-		}
-		boolean success = false;
-		if(!userExists) {
-			String sql2 = "INSERT INTO patients " + 
-						"VALUES (" + first_name + ", " + last_name + ");";
-			System.out.println("1=male, 2=female, 3=N/A");
-			VisitModel visit = new VisitModel();
-			visit.create();
-
-			try (ResultSet rs = db.executeQuery(sql3)) {
-				success = rs.next();
-			}finally {
-				db.close();
-			}
-		}
-		assertTrue(success);
-	}
-	}
-
 }
--- test/models/PatientModelTest.java
+++ test/models/PatientModelTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package models;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +16,6 @@ import java.sql.Timestamp;
 import adapters.DBAdapter;
 
 /**
- * @author darwingroskleg
  *
  */
 class PatientModelTest {
@@ -52,44 +48,31 @@ class PatientModelTest {
 	void tearDown() throws Exception {
 	}
 
-	// Broken example of how to test a unit
 	@Test
-	void fullNameTest() {
-		fail("Not yet implemented");
+	void testFindByID() throws SQLException {
+		PatientModel p = PatientModel.findByID(2);
+		assertEquals(2, p.getID());
+	}
+
+	@Test
+	void testFullName() {
 		PatientModel p = new PatientModel("John", "Smith");
-		assertEquals(p.fullName(), "John Smith");
+		assertEquals("John Smith", p.fullName());
 	}
 	
 	@Test
-	void testcreate() throws SQLException {
-		DBAdapter db = new DBAdapter();
-		boolean userExists = false;
-		String sql = 
-				"SELECT count(first_name), count(last_name)\n" + 
-				"FROM users " + 
-				"WHERE first_name = '" + first_name + "' " + 
-				"AND last_name = '" + last_name + "';";
-		try (ResultSet rs = db.executeQuery(sql)) {
-			userExists = rs.next();
-		}
-		boolean success = false;
-		if(!userExists) {
-			String sql1 = "INSERT INTO patients " + 
-						"VALUES (" + first_name + ", " + last_name + ");";
-			//display message on gender that lets user know 1=male, 2=female, 3=N/A
-			System.out.println("1=male, 2=female, 3=N/A");
-			String sql2 = "INSERT INTO visits " + 
-						"VALUES (" + gender + ", " + dob + ", " + check_in + ");";
-			try (ResultSet rs = db.executeQuery(sql1)) {
-				success = rs.next();
-			}
-			try (ResultSet rs = db.executeQuery(sql2)) {
-				success = rs.next();
-			}finally {
-				db.close();
-			}
-		}
-		assertTrue(success);
+	void testCreateDelete() throws SQLException {
+		PatientModel p = new PatientModel();
+		assertTrue(p.create());
+
+		// Cleanup
+		int id = p.getID();
+		assertTrue(p.delete());
+
+		// Double check
+		PatientModel p2 = PatientModel.findByID(id);
+		assertNotEquals(id, p2.getID());
 	}
 
+	
 }
