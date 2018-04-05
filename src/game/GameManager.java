94cf2daa950232d1db501319ea027ca56405a7ea
Darwin Groskleg <darwingroskleg@gmail.com>
2018-04-05 15:03:49 -0300

    Re-renames and arranges left side buttons Last of partial fixes to issue #27 Also moves the graph clearing button to the top

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -62,21 +62,21 @@ public class GameManager extends AbstractGame {
 
 		unauthorizedAlert = new PopUp("Unauthorized Action", 416, 64);
 		addUser = new PopUp("Add User", 256, 256);
-		addUser.addInput(96, 0, 128, 32, "First Name:");
+		addUser.addInput(96,  0, 128, 32, "First Name:");
 		addUser.addInput(96, 32, 128, 32, "Last Name:");
 		addUser.addInput(96, 64, 128, 32, "Email:");
 		addUser.addInput(96, 96, 128, 32, "Password:");
 		addUser.addCheckBox("HIPAA Authorized:", 156, 144, 12);
-		editUser = new PopUp("Edit User",256,128);
+		editUser = new PopUp("Password", 256, 128);
 		editUser.addInput(112, 0, 128, 32, "New Password:");
 		removeUser = new PopUp("Remove User", 256, 128);
 		removeUser.addInput(128, 0, 128, 32, "UserID to delete");
-		addPatient = new PopUp("Add Patient", 256, 256);
-		addPatient.addInput(96, 0, 128, 32, "First Name:");
-		addPatient.addInput(96, 32, 128, 32, "Last Name:");
-		addPatient.addInput(96, 64, 128, 32, "Address:");
-		addPatient.addInput(96, 96, 128, 32, "Gender(M,F,U)");
-		addPatient.addInput(96, 128, 128, 32, "DOB(m/d/y):");
+		addPatient = new PopUp("Add Patient", 320, 256);
+		addPatient.addInput(176,  0, 128, 32, "First Name:");
+		addPatient.addInput(176,  32, 128, 32, "Last Name:");
+		addPatient.addInput(176,  64, 128, 32, "Address:");
+		addPatient.addInput(176,  96, 128, 32, "Gender (M=1,F=2,?=3):");
+		addPatient.addInput(176, 128, 128, 32, "DOB (mm/dd/yyyy):");
 
 		search = new AttributeSearch(1,32,142,32);
 		//page = new Page("test", new Attribute[] {new Attribute("I went to the shop to count up how dumb i am for using this hose in the wrong spot one day i thought i was a nod","")});
@@ -91,12 +91,15 @@ public class GameManager extends AbstractGame {
 		leftSide.getLastTabAdded().getSections().get(0).addCheckbox("Female:", 128, 64, 8);
 		leftSide.getLastTabAdded().getSections().get(0).addInput("Min Age:", 64, 96, 78, 32);
 		leftSide.getLastTabAdded().getSections().get(0).addInput("Max Age:", 64, 128, 78, 32);
-		leftSide.addButton("Add User", 256, 64, 0, false);
-		leftSide.addButton("Edit", 320, 64, 0, false);
-		leftSide.addButton("Delete User", 384, 64, 0, false);
-		leftSide.addButton("Add Patient", 444, 64, 0, false);
-		leftSide.addButton("Reset Graph", 508, 64, 0, false);
+		leftSide.addButton("Reset Graph", 193, 64, 0, false);
 
+		leftSide.addButton("Add Patient", 288, 64, 0, false);
+
+		leftSide.addButton("Add User",    416, 64, 0, false);
+		leftSide.addButton("Delete User", 480, 64, 0, false);
+
+		leftSide.addButton("Password", GameContainer.height - (65*2), 64, 0, false);
+		leftSide.addButton("Logout",   GameContainer.height - 65,     64, 0, false);
 	}
 
 	private void rightSideInit() {
@@ -195,7 +198,12 @@ public class GameManager extends AbstractGame {
 			addUser.update(gc, dt);
 		}
 		if(addUser.shouldClose() && !addUser.isClosed()) {
-			UserModel newUser = new UserModel(addUser.getStringFromInput(0),addUser.getStringFromInput(1),addUser.getStringFromInput(2),addUser.getStringFromInput(3),addUser.boxTicked("HIPAA Authorized:"));
+			UserModel newUser = new UserModel(
+					addUser.getStringFromInput(0),
+					addUser.getStringFromInput(1),
+					addUser.getStringFromInput(2),
+					addUser.getStringFromInput(3),
+					addUser.boxTicked("HIPAA Authorized:") );
 			try {
 				if(newUser.create()) {
 					addUser.clearTexts();
@@ -207,7 +215,9 @@ public class GameManager extends AbstractGame {
 			addUser.close();
 		}
 		search.update(gc, filter, dt);
-		if(editUser.shouldClose() && !editUser.isClosed()) {
+
+		// Editing the current user (change password)
+		if (editUser.shouldClose() && !editUser.isClosed()) {
 			try {
 				CURRENT_USER.changePassword(editUser.getStringFromInput(0));
 				editUser.clearTexts();
@@ -216,9 +226,8 @@ public class GameManager extends AbstractGame {
 			}
 			editUser.close();
 		}
-		if(!editUser.isClosed()) {
-			editUser.update(gc, dt);
-		}
+		if (!editUser.isClosed()) editUser.update(gc, dt);
+
 		if(removeUser.shouldClose() && !removeUser.isClosed()) {
 			try {
 				UserModel u = UserModel.findByID(Integer.parseInt(removeUser.getStringFromInput(0)));
@@ -283,7 +292,7 @@ public class GameManager extends AbstractGame {
 			else
 				unauthorizedAlert.open();
 		}
-		if (leftSide.getTab(0).isButtonActive("Edit")) {
+		if (leftSide.getTab(0).isButtonActive("Password")) {
 			editUser.open();
 		}
 		if (leftSide.getTab(0).isButtonActive("Delete User")) {
