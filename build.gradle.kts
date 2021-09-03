840bae75553c0a279889973b7d054d8ef0107e50
Darwin Jacob Groskleg <darwingroskleg@gmail.com>
2021-09-03 02:50:51 -0400

    Adds gradle config for straightforward running of tests and dependency management

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+
+plugins {
+  java
+  `java-library`
+//  application
+}
+
+repositories {
+  mavenCentral()
+}
+
+group = "ca.darweb"
+version = "0.2"
+
+dependencies {
+  // https://mvnrepository.com/artifact/com.oracle.database.jdbc/ojdbc8
+  implementation("com.oracle.database.jdbc:ojdbc8:21.3.0.0")
+
+  testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+}
+
+sourceSets {
+  main {
+    java {
+      setSrcDirs(listOf("src"))
+    }
+  }
+
+  test {
+    java {
+      setSrcDirs(listOf("test"))
+    }
+  }
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
+
+//tasks.withType(JavaCompile::class) {
+//  options.compilerArgs.add("-Xlint:deprecation")
+//  options.compilerArgs.add("-Xlint:unchecked")
+//}
+
+//application {
+//  // Define the main class for the application.
+//  mainClass.set("MRIDB.App")
+//}
